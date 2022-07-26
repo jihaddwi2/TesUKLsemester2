a1b2159eaa5b73c18ffdca40f5c596010f9e3822
jihaddwi2 <jiahdaji91@gmail.com>
2022-07-26 10:59:32 +0700

    latihan

---
+++ src/Mani.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ACER
+ */
+
+import java.util.ArrayList;
+
+public class Mani {
+
+    /**
+     * @param args the command line arguments
+     */
+        
+    public static void main(String[] args) {
+       
+        ArrayList obj = new ArrayList();
+    
+    obj.add("maman");
+    obj.add("19");
+    obj.add("12831207");
+    
+    
+    obj.remove(17);
+    
+        System.out.println(obj);
+    }
+    
+}
